0ce56b884739edecdb5a48893675b3b4549d3e4c
heyh <1364241072@qq.com>
2024-04-24 13:28:08 +0800

    单发拨弹写出来了没试,小改逻辑

--- src/ATTACK.cpp
+++ src/ATTACK.cpp
@@ -17,40 +17,41 @@ int accum = 0;
     //先写单发模式拨弹
     while(1)
     {
+        if (accum >= 3)//卡弹检测(有空用函数封装一下)
+        {
+            if (flag == 1)
+            {
+                ATTACK[0].DATA.AIM -= 2r;
+                flag = 0;
+                accum = 0;
+            }
+
+            if(flag == 0)
+            {
+                ATTACK[0].DATA.AIM += 2r;
+                flag = 1;
+                accum = 0;
+            }
+        }
 
         if (dbus_data.REMOTE.s1_u8 == 1)
         {
+            while(1)
             if (dbus_data.REMOTE.s1_u8 == 3)
-            {   //卡弹检测(有空用函数封装一下)
-                if (accum >= 3)
-                {
-                    if (flag == 1)
-                    {
-                        ATTACK[0].DATA.AIM -= 2r;
-                        flag = 0;
-                        accum = 0;
-                    }
-
-                    if(flag == 0)
-                    {
-                        ATTACK[0].DATA.AIM += 2r;
-                        flag = 1;
-                        accum = 0;
-                    }
-                }
-                else
-                {
+            {
                     ATTACK[0].DATA.ANGLE_LAST = ATTACK[0].DATA.ANGLE_NOW;
                     ATTACK[0].DATA.AIM += 2r;//弹丸直径待测量
 
                     if (ATTACK[0].DATA.ANGLE_NOW - ATTACK[0].DATA.ANGLE_LAST < 2r)
                         accum++;
-                }
+                break;
             }
-            //卡弹检测
+
 
 
         }
+        else
+            break;
         YUN_F_CAN_SEND( );
 
     }
