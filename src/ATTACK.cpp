1869b935ccffca471f8b338169a70116164037cb
heyh <1364241072@qq.com>
2024-04-25 18:30:49 +0800

    加了PID和摩擦轮，还没测试

--- src/ATTACK.cpp
+++ src/ATTACK.cpp
@@ -5,7 +5,7 @@
 #include "can.h"
 #include "main.h"
 #include "MOTOR.h"
-
+#include "PID.h"
 
 YUN_TYPEDEF_MOTOR ATTACK[3];
 //0：拨弹 1：摩擦轮左 2：摩擦轮右
@@ -13,15 +13,22 @@ int accum = 0;
 //退弹标志位
 [[noreturn]] void YUN_F_ATTACK_THREAD()
 {
+    ATTACK[2].DATA.AIM = 3000;
+    ATTACK[3].DATA.AIM = -3000;
     int flag = 0;
     //先写单发模式拨弹
     while(1)
     {
+        YUN_F_MOTOR_PID_SC(&ATTACK[2]);
+        YUN_F_MOTOR_PID_SC(&ATTACK[3]);
+
         if (accum >= 3)//卡弹检测(有空用函数封装一下)
         {
             if (flag == 1)
             {
                 ATTACK[0].DATA.AIM -= 2r;
+                YUN_F_MOTOR_PID_GIMBAL(&ATTACK[1],ATTACK[1].DATA.ANGLE_NOW,ATTACK[1].DATA.SPEED_NOW);
+
                 flag = 0;
                 accum = 0;
             }
@@ -29,6 +36,8 @@ int accum = 0;
             if(flag == 0)
             {
                 ATTACK[0].DATA.AIM += 2r;
+                YUN_F_MOTOR_PID_GIMBAL(&ATTACK[1],ATTACK[1].DATA.ANGLE_NOW,ATTACK[1].DATA.SPEED_NOW);
+
                 flag = 1;
                 accum = 0;
             }
@@ -40,10 +49,13 @@ int accum = 0;
             if (dbus_data.REMOTE.s1_u8 == 3)
             {
                     ATTACK[0].DATA.ANGLE_LAST = ATTACK[0].DATA.ANGLE_NOW;
-                    ATTACK[0].DATA.AIM += 2r;//弹丸直径待测量
+                    ATTACK[0].DATA.AIM += 2r;//弹丸直径待 测量
 
-                    if (ATTACK[0].DATA.ANGLE_NOW - ATTACK[0].DATA.ANGLE_LAST < 2r)//检测卡弹
-                        accum++;
+                if (ATTACK[0].DATA.ANGLE_NOW - ATTACK[0].DATA.ANGLE_LAST < 2r)//检测卡弹
+                    accum++;
+                YUN_F_MOTOR_PID_GIMBAL(&ATTACK[1],ATTACK[1].DATA.ANGLE_NOW,ATTACK[1].DATA.SPEED_NOW);
+                YUN_F_CAN_SEND();
+
                 break;
             }
 
