6c6683bf03a8d68a5a457aba9b7a05cf952a269b
heyh <1364241072@qq.com>
2024-05-02 21:42:12 +0800

    单发连发完成，卡弹未完成

--- src/ATTACK.cpp
+++ src/ATTACK.cpp
@@ -10,35 +10,38 @@
 #include "MATH.h"
 #include "GIMBAL.h"
 
-YUN_TYPEDEF_MOTOR ATTACK[4]= { };
+YUN_TYPEDEF_MOTOR ATTACK[4]= {0};
 YUN_TYPEDEF_TOP TEST_1{ };
 ////0：拨弹 1：摩擦轮左 2：摩擦轮右
 
 ////退弹标志位
-const float PID_P[5] = {4,0.001,0,3000,3000};
-const float PID_S[5] = {4,0.001,0,3000,3000};
+const float PID_P[5] = {1,0,0,3000,1000};
+const float PID_S[5] = {5,0,0,3000,3000};
 
 int jam_time = 0;
 int flag = 0;
 int single_lock = 0;
-
+int32_t jam_angle = 0;
 
 int YUN_F_JAM( )
 {
-    YUN_F_CAN_RECEIVE(ATTACK,&TEST_1,YUN_D_CAN_2);
-    if (jam_time > 3000 && ATTACK[0].DATA.SPEED_NOW <= 200 && ATTACK[0].DATA.ANGLE_NOW - ATTACK[0].DATA.ANGLE_LAST<=200 &&flag == -1 )
+
+//    && (ATTACK[0].DATA.ANGLE_INFINITE - jam_angle<=50000)
+    if ((jam_time > 300) && (ATTACK[0].DATA.SPEED_NOW <= 200)  &&(flag == 1) && (ATTACK[0].DATA.ANGLE_INFINITE - jam_angle<=50000))
     {
-        ATTACK[0].DATA.AIM += 200;
-        YUN_F_MOTOR_PID_GIMBAL(&ATTACK[0],ATTACK[0].DATA.ANGLE_NOW,ATTACK[0].DATA.SPEED_NOW);
+//        printf("flag:%d",-1);
+        ATTACK[0].DATA.AIM =(float )ATTACK[0].DATA.ANGLE_INFINITE + 3600000.0f;
         flag = 0;
+        jam_time = 0;
         return false;
     }
 
-    if (jam_time > 3000 && ATTACK[0].DATA.SPEED_NOW <= 200 && ATTACK[0].DATA.ANGLE_NOW - ATTACK[0].DATA.ANGLE_LAST<=200 && flag == 0)
+    if ((jam_time > 300) && (ATTACK[0].DATA.SPEED_NOW <= 200) && (flag == 0)&& (ATTACK[0].DATA.ANGLE_INFINITE - jam_angle<=50000))
     {
-        ATTACK[0].DATA.AIM -= 200;
-        YUN_F_MOTOR_PID_GIMBAL(&ATTACK[0],ATTACK[0].DATA.ANGLE_NOW,ATTACK[0].DATA.SPEED_NOW);
+//        printf("flag:%d",0);
+        ATTACK[0].DATA.AIM = (float )ATTACK[0].DATA.ANGLE_INFINITE - 3600000.0f;
         flag = 1;
+        jam_time = 0;
         return false;
     }
 
@@ -48,56 +51,68 @@ int YUN_F_JAM( )
     }
 }
 
-void YUN_F_SIGLE()
-{
 
 
-}
+//有空把单发封装一下，有点多了
+[[noreturn]] void YUN_F_ATTACK_THREAD( ) {
+    YUN_F_PID_INIT(&ATTACK[0].PID_P, PID_P, 1000, 3000);
+    YUN_F_PID_INIT(&ATTACK[0].PID_S, PID_S, 1000, 3000);
+    jam_time = 0;
+//    printf("PID %f %f %f\n",ATTACK[0].PID_S.IN.P,ATTACK[0].PID_S.IN.I_LIT,ATTACK[0].PID_S.IN.ALL_LIT);
+    while (1) {
+        YUN_F_CAN_RECEIVE(ATTACK, &TEST_1, YUN_D_CAN_2);
 
-
-
-[[noreturn]] void YUN_F_ATTACK_THREAD( )
-{
-    while (1)
-    {
-        YUN_F_PID_INIT(&ATTACK[0].PID_P,PID_P,1000,3000);
-        YUN_F_PID_INIT(&ATTACK[0].PID_P,PID_P,1000,3000);
         if (YUN_F_JAM())//没堵弹
         {
-            if(dbus_data.REMOTE.s1_u8 == 2 && single_lock == 0)//单发不上锁
+            if (dbus_data.REMOTE.s1_u8 == 3)//单发解锁
             {
-                ATTACK[0].DATA.AIM = ATTACK[0].DATA.ANGLE_INFINITE + 200;
-                YUN_F_MOTOR_PID_GIMBAL(&ATTACK[0],ATTACK[0].DATA.ANGLE_NOW,ATTACK[0].DATA.SPEED_NOW);
-                jam_time = 0;
-                single_lock = 1;
+                single_lock = 0;
+//                ATTACK[0].DATA.AIM = ATTACK[0].DATA.ANGLE_INFINITE;//存疑，我觉得目标值下降是因为这句话，电机还没转到目标值程序就运行到这，然后把目标值更新为中途的角度
+                //单发感觉不需要上边这句话，连发为了方便随时停止才需要
             }
-            else if(dbus_data.REMOTE.s1_u8 == 2 && single_lock == 1)//单发上锁
+//        if (dbus_data.REMOTE.s1_u8 == )
+            if ((dbus_data.REMOTE.s1_u8 == 1) &&(jam_time < 500))//单发不上锁
             {
-                //Do noting
-            }
+                if (single_lock == 0) {
+                    ATTACK[0].DATA.AIM = (float) ATTACK[0].DATA.ANGLE_INFINITE + 3600050.0f;
+                    single_lock = 1;
 
-            else if (dbus_data.REMOTE.s1_u8 == 1)//连发
-            {
-                ATTACK[0].DATA.AIM = ATTACK[0].DATA.AIM = ATTACK[0].DATA.ANGLE_INFINITE + 200;
-                YUN_F_MOTOR_PID_GIMBAL(&ATTACK[0],ATTACK[0].DATA.ANGLE_NOW,ATTACK[0].DATA.SPEED_NOW);
+                    }
 
             }
+//3600050.0f;
+//            if(dbus_data.REMOTE.s1_u8 == 3)
+//            {
+//            ATTACK[0].DATA.AIM = ATTACK[0].DATA.ANGLE_INFINITE;
+//            }
+            else if ((dbus_data.REMOTE.s1_u8 == 2)&& (jam_time < 500))//连发
+            {
+
+                ATTACK[0].DATA.AIM = (float) ATTACK[0].DATA.ANGLE_INFINITE + 36000.0f;
+
+            }
+
+
+            if (ATTACK[0].DATA.SPEED_NOW < 200 )
+            {
+                jam_time ++ ;
+                jam_angle = ATTACK[0].DATA.ANGLE_INFINITE;
+            }
+
+
+            //卡弹时间积累：排除不发弹情况和单发上锁情况
+
+//            if ((ATTACK[0].DATA.SPEED_NOW < 200) && (dbus_data.REMOTE.s1_u8 == 1) && (single_lock != 1)) {
+//                jam_time += 20;
+//                jam_angle = ATTACK[0].DATA.ANGLE_INFINITE;
+//            }
+            YUN_F_MOTOR_PID_GIMBAL(&ATTACK[0], ATTACK[0].DATA.ANGLE_INFINITE, ATTACK[0].DATA.SPEED_NOW);
+            YUN_F_CAN_SEND(YUN_D_CAN_2, 0x200, 0, 0, (int16_t) ATTACK[0].PID_S.out.ALL_OUT, 0);
+//        printf("MOD %d LOCK %d ANGLE %d AIM %f\n",dbus_data.REMOTE.s1_u8,single_lock,ATTACK[0].DATA.ANGLE_INFINITE,ATTACK[0].DATA.AIM);
+//        printf("%d\n",jam_time);
+//        printf("%d\n",ATTACK[0].DATA.ANGLE_INFINITE);
 
         }
 
-        //卡弹时间积累：排除不发弹清空和单发上锁情况
-        if((ATTACK[0].DATA.SPEED_NOW < 200 && dbus_data.REMOTE.s1_u8 != 3) ||(ATTACK[0].DATA.SPEED_NOW < 200 && dbus_data.REMOTE.s1_u8 == 2 && single_lock == 1) )
-        {
-            jam_time ++;
-            ATTACK[0].DATA.ANGLE_LAST = ATTACK[0].DATA.ANGLE_INFINITE;
-        }
-
-        YUN_F_CAN_SEND(YUN_D_CAN_2,0x200,0,0,ATTACK[0].PID_S.out.ALL_OUT,0);
-
-        if (dbus_data.REMOTE.s1_u8 == 3)//单发解锁
-        {
-            single_lock = 0;
-        }
     }
-
 }
